a3fffd372a5d7996ab2fc95600136ab0c123958d
tr1pster <160784831+tr1pster@users.noreply.github.com>
2024-04-27 17:19:09 +0200

    cleaning code

--- deployArtGallery.js
+++ deployArtGallery.js
@@ -1,7 +1,10 @@
 require("@nomiclabs/hardhat-waffle");
 require("dotenv").config();
+const hre = require("hardhat");
+
 const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
 const RPC_URL = process.env.RPC_URL || "";
+
 module.exports = {
   solidity: "0.8.4",
   networks: {
@@ -11,15 +14,31 @@ module.exports = {
     },
   },
 };
-const hre = require("hardhat");
+
 async function main() {
+  await compileContracts();
+  const artGallery = await deployArtGallery();
+  await logDeploymentDetails(artGallery);
+}
+
+async function compileContracts() {
+  console.log("Compiling contracts...");
   await hre.run('compile');
-  const ArtGallery = await hre.ethers.getContractFactory("ArtGallery");
+  console.log("Compilation finished.");
+}
+
+async function deployArtGallery() {
   console.log("Deploying ArtGallery...");
+  const ArtGallery = await hre.ethers.getContractFactory("ArtGallery");
   const artGallery = await ArtGallery.deploy();
   await artGallery.deployed();
+  return artGallery;
+}
+
+async function logDeploymentDetails(artGallery) {
   console.log("ArtGallery deployed to:", artGallery.address);
 }
+
 main()
   .then(() => process.exit(0))
   .catch((error) => {
