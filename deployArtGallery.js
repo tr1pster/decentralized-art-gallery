89e4a3bcab93c2eee35d3eea351ea2a62e2b1523
tr1pster <160784831+tr1pster@users.noreply.github.com>
2024-05-02 13:42:54 +0200

    update with new function

--- deployArtGallery.js
+++ deployArtGallery.js
@@ -1,9 +1,12 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
 require("dotenv").config();
+
 const hardhatRuntimeEnvironment = require("hardhat");
 
 const WALLET_PRIVATE_KEY = process.env.PRIVATE_KEY || "";
 const BLOCKCHAIN_RPC_URL = process.env.RPC_URL || "";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
 
 module.exports = {
   solidity: "0.8.4",
@@ -13,12 +16,16 @@ module.exports = {
       accounts: [WALLET_PRIVATE_KEY],
     },
   },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
 };
 
 async function main() {
   await compileSmartContracts();
   const deployedArtGalleryContract = await deployArtGalleryContract();
   await displayContractDeploymentDetails(deployedArtGalleryContract);
+  await verifyContractOnEtherscan(deployedArtGalleryContract.address);
 }
 
 async function compileSmartContracts() {
@@ -32,11 +39,25 @@ async function deployArtGalleryContract() {
   const ArtGalleryContract = await hardhatRuntimeEnvironment.ethers.getContractFactory("ArtGallery");
   const deployedContract = await ArtGalleryContract.deploy();
   await deployedContract.deployed();
+  console.log("Deployment completed. Contract address:", deployedContract.address);
   return deployedContract;
 }
 
 async function displayContractDeploymentDetails(deployedContract) {
   console.log("ArtGallery Smart Contract deployed to:", deployedContract.address);
+}
+
+async function verifyContractOnEtherscan(contractAddress) {
+  console.log(`Verifying contract on Etherscan...`);
+  try {
+    await hardhatRuntimeEnvironment.run("verify:verify", {
+      address: contractAddress,
+      network: "ropsten",
+    });
+    console.log("Verification successful!");
+  } catch (error) {
+    console.error("Verification failed:", error);
+  }
 }
 
 main()
